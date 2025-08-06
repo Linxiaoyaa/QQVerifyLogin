62043fabb9c42346d8dd80accf116c9b6bc80075
Linxiaoya <11208120@qq.com>
2025-08-06 23:02:52 +0800

    1.0.2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod route;
 mod handle;
 mod mystruct;
+mod route;
 use crate::route::creatroute;
 
 use std::net::SocketAddr;
@@ -11,24 +11,24 @@ async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
         )
         .init();
 
-
-
-    let addr = SocketAddr::from(([0,0,0,0],3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::info!("服务器监听于 {}", addr);
     tracing::info!("通过/verify/submit_ticket来进行第一步验证");
     tracing::info!("通过/verify/submit_phone_number来进行第二步验证");
     tracing::info!("通过/verify/submit_auth来进行第三步验证");
     tracing::info!("Github：https://github.com/Linxiaoyaa/QQVerifyLogin");
     tracing::info!("作者：Linxiaoyaa");
+    tracing::info!("交流群：957874828");
     tracing::info!("感谢你的使用，如果帮助到了你，请star一下吧~");
-    let http_listener=tokio::net::TcpListener::bind(addr).await.unwrap();
+    let http_listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(
         http_listener,
-        creatroute().into_make_service_with_connect_info::<SocketAddr>()
-    ).await.unwrap()
+        creatroute().into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap()
 }
