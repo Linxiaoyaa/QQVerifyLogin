e977d7d71768029cce076f51b445d2d8eef83c53
Linxiaoya <11208120@qq.com>
2025-09-09 00:06:44 +0800

    更新字段

--- src/mystruct.rs
+++ src/mystruct.rs
@@ -1,17 +1,16 @@
+use axum::Json;
 use axum::response::{IntoResponse, Response};
 use serde::{Deserialize, Serialize};
-use axum::Json;
-#[derive(Debug,Deserialize,Serialize,Clone)]
-pub struct APIResult<T:Serialize>{
-    pub code:i64,
-    pub reqststus:String,
-    pub data:T,
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct APIResult<T: Serialize> {
+    pub code: i64,
+    pub reqststus: String,
+    pub data: T,
 }
 
-impl<T:Serialize> IntoResponse for APIResult<T> {
+impl<T: Serialize> IntoResponse for APIResult<T> {
     fn into_response(self) -> Response {
         let jsonbody = Json(self);
         jsonbody.into_response()
     }
-
 }
