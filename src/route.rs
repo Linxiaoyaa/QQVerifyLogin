d93ee157c0e4dedc75873ad980e1656523a5c04c
Linxiaoya <11208120@qq.com>
2025-09-09 00:06:53 +0800

    更新字段

--- src/route.rs
+++ src/route.rs
@@ -1,12 +1,10 @@
+use crate::handle::{login_verify1, login_verify2, login_verify3};
 use axum::Router;
 use axum::routing::post;
-use crate::handle::{login_verify1, login_verify2,login_verify3};
 
-pub fn  creatroute()-> Router {
-          Router::new()
-              .route("/verify/submit_ticket",post(login_verify1))
-              .route("/verify/submit_phone_number",post(login_verify2))
-              .route("/verify/submit_auth",post(login_verify3))
-    
+pub fn creatroute() -> Router {
+    Router::new()
+        .route("/verify/submit_ticket", post(login_verify1))
+        .route("/verify/submit_phone_number", post(login_verify2))
+        .route("/verify/submit_auth", post(login_verify3))
 }
-
